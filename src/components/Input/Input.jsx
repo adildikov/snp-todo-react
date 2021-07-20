29059ffb6c5098a075394fac47a147d155672341
AlekseyDildikov <adildikov@gmail.com>
2021-07-20 17:58:03 +0300

    Actually Input done

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Input(props) {
+  const [message, setMessage] = useState("");
+
+  const handlePressed = (e) => {
+    if (e.key === "Enter") {
+      console.log(message);
+      setMessage("");
+    }
+  };
+
+  const handleClick = () => {
+    console.log(message);
+    setMessage("");
+  };
+
   return (
     <div className="main_addTodoArea">
       <input
         type="text"
+        value={message}
         className="addTodoArea__newTodo"
         placeholder="Add a new case"
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyPress={(e) => handlePressed(e)}
       />
-      <button className="addTodoArea__addTodo button">Add</button>
+      <button className="addTodoArea__addTodo button" onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 }
